740e709ac479ec17b5d37630a666b969f2095cc7
h493 <chhikarahimanshu21@gmail.com>
2026-06-28 16:00:32 +0530

    spring ai integration using ollama

---
+++ src/test/java/com/example/learn_spring_ai/service/AIServiceTest.java
@@ -0,0 +1,18 @@
+package com.example.learn_spring_ai.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AIServiceTest {
+
+    @Autowired
+    private AIService aiService;
+
+    @Test
+    public void testGetJoke(){
+        var joke = aiService.getJoke("Dogs");
+        System.out.println(joke);
+    }
+}

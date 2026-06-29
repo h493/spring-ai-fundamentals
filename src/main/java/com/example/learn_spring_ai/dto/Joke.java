592adbd9c368fa89aae289a0055c05fc44f1915a
h493 <chhikarahimanshu21@gmail.com>
2026-06-29 15:50:09 +0530

    getting customised response

---
+++ src/main/java/com/example/learn_spring_ai/dto/Joke.java
@@ -0,0 +1,9 @@
+package com.example.learn_spring_ai.dto;
+
+public record Joke(
+        String text,
+        String category,
+        Double laughScore,
+        Boolean isNSFW
+) {
+}
--- src/main/java/com/example/learn_spring_ai/service/AIService.java
+++ src/main/java/com/example/learn_spring_ai/service/AIService.java
@@ -1,8 +1,13 @@
 package com.example.learn_spring_ai.service;
 
+import com.example.learn_spring_ai.dto.Joke;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -11,10 +16,28 @@ public class AIService {
     private final ChatClient chatClient;
 
     public String getJoke(String topic){
-       return chatClient.prompt()
-               .system("You are a sarcastic joker, give response in one line.")
-                .user("Give me a joke on a topic: " + topic)
+
+        String systemPrompt = """
+                you are a sarcastic joker , but don't make jokes about politics.
+                Give a joke on the topic : {topic}
+                """;
+
+        PromptTemplate promptTemplate = new PromptTemplate(systemPrompt);
+        String renderText = promptTemplate.render(Map.of("topic", topic));
+
+        var response = chatClient.prompt()
+                .user(renderText)
+                .advisors(new SimpleLoggerAdvisor())
                 .call()
-                .content();
+                .entity(Joke.class);
+
+
+//       return chatClient.prompt()
+//               .system("You are a sarcastic joker, give response in one line.")
+//                .user("Give me a joke on a topic: " + topic)
+//                .call()
+//                .content();
+
+        return response.text();
     }
 }
